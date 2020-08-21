20070ae784ab207173bb2a5295b38980d8f1f845
asugi <road.83@hotmail.fr>
2020-08-21 11:51:37 +0200

    modif noms variables objetbg-image

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 
 let url = "https://www.prevision-meteo.ch/services/json/";
 let request = fetch("https://www.prevision-meteo.ch/services/json/toulon")
-const restTemp = document.querySelector("#resTemp");
-const restIcon = document.querySelector("#resIcon");
-const restCity = document.querySelector("#resCity");
-const restDate = document.querySelector("#resDate");
-const restWindDir = document.querySelector("#windDirection");
-const restWindSpeed = document.querySelector("#windSpeed");
-const title = document.querySelector("#title");
-const restWeather = document.querySelector("#weather-result");
-let restHourly = document.querySelector("#hourlyData");
+const $temp = document.querySelector("#resTemp");
+const $icon = document.querySelector("#resIcon");
+const $city = document.querySelector("#resCity");
+const $date = document.querySelector("#resDate");
+const $windDir = document.querySelector("#windDirection");
+const $windSpeed = document.querySelector("#windSpeed");
+const $weather = document.querySelector("#weather-result");
+const $hourly = document.querySelector("#hourlyData");
 const $body = document.querySelector("#body");
-let condition ="";
 
         request.then(function(response){
             return response.json();
@@ -28,7 +26,7 @@ input.addEventListener("keyup", function(event) {
   }
 });
         
-function updateMeteo(){
+function updateWeather(){
     let cityWanted = document.getElementById("searchInput").value;
     fetch(url + cityWanted)
     .then(response => response.json())
@@ -40,129 +38,59 @@ function updateMeteo(){
     });
 }
 
-const updateBackground = function() {
-    switch (condition) {
-        case 'Ensoleillé':
-            $body.style.backgroundImage = 'url(https://observatorcl.info/wp-content/uploads/2020/01/ANM.jpg)';  
-            break;
-        case 'Ciel voilé':
-            $body.style.backgroundImage = 'url(https://images7.alphacoders.com/680/thumb-1920-680835.jpg)';
-            break;
-        case 'Faiblement nuageux':
-            $body.style.backgroundImage = 'url(https://images5.alphacoders.com/431/thumb-1920-431971.jpg)';
-            break;
-        case 'Eclaircies':
-            $body.style.backgroundImage = 'url(https://wallpaperaccess.com/full/1167968.jpg)';
-            break;
-        case 'Stratus se dissipant':
-            $body.style.backgroundImage = 'url(https://images7.alphacoders.com/680/thumb-1920-680835.jpg)';
-            break;
-        case 'Fortement nuageux':
-            $body.style.backgroundImage = 'url(https://images4.alphacoders.com/102/thumb-1920-1028191.jpg)';
-            break;
-        case 'Développement nuageux':
-            $body.style.backgroundImage = 'url(https://images4.alphacoders.com/913/thumb-1920-913354.jpg)';
-            break;
-        case 'Stratus':
-            $body.style.backgroundImage = 'url(https://images5.alphacoders.com/431/thumb-1920-431971.jpg)';
-            break;
-        case 'Nuit claire':
-            $body.style.backgroundImage = 'url(https://images3.alphacoders.com/824/thumb-1920-824826.jpg)';
-            break;
-        case 'Nuit bien dégagée':
-            $body.style.backgroundImage = 'url(https://images5.alphacoders.com/511/thumb-1920-511222.jpg)';
-            break;
-        case 'Nuit légèrement voilée':
-            $body.style.backgroundImage = 'url(https://images3.alphacoders.com/161/thumb-1920-161369.jpg)';
-            break;
-        case 'Nuit claire et stratus':
-            $body.style.backgroundImage = 'url(https://images8.alphacoders.com/676/thumb-1920-676644.jpg)';
-            break;
-        case 'Nuit nuageuse':
-            $body.style.backgroundImage = 'url(https://images7.alphacoders.com/937/thumb-1920-937486.jpg)';
-            break;
-        case 'Nuit avec développement nuageux':
-            $body.style.backgroundImage = 'url(https://images5.alphacoders.com/992/thumb-1920-992126.jpg)';
-            break;
-        case 'Averses de pluie faible':
-            $body.style.backgroundImage = 'url(https://images8.alphacoders.com/714/thumb-1920-714894.jpg)';
-            break;
-        case 'Pluie faible':
-            $body.style.backgroundImage = 'url(https://images3.alphacoders.com/986/thumb-1920-986363.jpg)';
-            break;
-        case 'Averses de pluie modérée':
-            $body.style.backgroundImage = 'url(https://images5.alphacoders.com/312/thumb-1920-312372.jpg)';
-            break;
-        case 'Averses de pluie forte':
-            $body.style.backgroundImage = 'url(https://images.alphacoders.com/108/thumb-1920-1084974.jpg)';
-            break;
-        case 'Couvert avec averses':
-            $body.style.backgroundImage = 'url(https://images2.alphacoders.com/783/thumb-1920-783245.jpg)';
-            break;
-        case 'Pluie forte':
-            $body.style.backgroundImage = 'url(https://images2.alphacoders.com/597/thumb-1920-597905.jpg)';
-            break;
-        case 'Pluie modérée':
-            $body.style.backgroundImage = 'url(https://images7.alphacoders.com/103/thumb-1920-1036817.jpg)';
-            break;
-        case 'Faiblement orageux':
-            $body.style.backgroundImage = 'url(https://images6.alphacoders.com/854/thumb-1920-854198.jpg)';
-            break;
-        case 'Orage modéré':
-            $body.style.backgroundImage = 'url(https://images.alphacoders.com/453/thumb-1920-453238.jpg)';
-            break;
-        case 'Fortement orageux':
-            $body.style.backgroundImage = 'url(https://images2.alphacoders.com/596/thumb-1920-596907.jpg)';
-            break;
-        case 'Nuit faiblement orageuse':
-            $body.style.backgroundImage = 'url(https://images8.alphacoders.com/697/thumb-1920-697542.jpg)';
-            break;
-        case 'Averses de neige faible':
-            $body.style.backgroundImage = 'url(https://images5.alphacoders.com/341/thumb-1920-341015.jpg)';
-            break;
-        case 'Neige faible':
-            $body.style.backgroundImage = 'url(https://images3.alphacoders.com/116/thumb-1920-116827.jpg)';
-            break;
-        case 'Neige modérée':
-            $body.style.backgroundImage = 'url(https://images7.alphacoders.com/876/thumb-1920-876256.jpg)';
-            break;
-        case 'Neige forte':
-            $body.style.backgroundImage = 'url(https://images.alphacoders.com/106/thumb-1920-106423.jpg)';
-            break;
-        case 'Pluie et neige mêlée faible':
-            $body.style.backgroundImage = 'url(https://images.alphacoders.com/106/thumb-1920-106423.jpg)';
-            break;
-        case 'Pluie et neige mêlée modérée':
-            $body.style.backgroundImage = 'url(https://images6.alphacoders.com/899/thumb-1920-899414.jpg)';
-            break;
-        case 'Pluie et neige mêlée forte':
-            $body.style.backgroundImage = 'url(https://images6.alphacoders.com/793/thumb-1920-793091.jpg)';
-            break;
-        case 'Nuit avec averses de neige faible':
-            $body.style.backgroundImage = 'url(https://images2.alphacoders.com/814/thumb-1920-814106.jpg)';
-            break;
-        case 'Brouillard':
-            $body.style.backgroundImage = 'url(https://images3.alphacoders.com/929/thumb-1920-929605.jpg)';
-            break;
-        default:
-            $body.style.backgroundImage = 'url(https://images.alphacoders.com/143/thumb-1920-143073.jpg)';  
-            break;
+const imagesForConditions = {
+        'Ensoleillé':'https://observatorcl.info/wp-content/uploads/2020/01/ANM.jpg',
+        'Ciel voilé':'https://images7.alphacoders.com/680/thumb-1920-680835.jpg',
+        'Faiblement nuageux':'https://images5.alphacoders.com/431/thumb-1920-431971.jpg',
+        'Eclaircies':'https://wallpaperaccess.com/full/1167968.jpg',
+        'Stratus se dissipant':'https://images7.alphacoders.com/680/thumb-1920-680835.jpg',
+        'Fortement nuageux':'https://images4.alphacoders.com/102/thumb-1920-1028191.jpg',
+        'Développement nuageux':'https://images4.alphacoders.com/913/thumb-1920-913354.jpg',
+        'Stratus':'https://images5.alphacoders.com/431/thumb-1920-431971.jpg',
+        'Nuit claire':'https://images3.alphacoders.com/824/thumb-1920-824826.jpg',
+        'Nuit bien dégagée':'https://images5.alphacoders.com/511/thumb-1920-511222.jpg',
+        'Nuit légèrement voilée':'https://images3.alphacoders.com/161/thumb-1920-161369.jpg',
+        'Nuit claire et stratus':'https://images8.alphacoders.com/676/thumb-1920-676644.jpg',
+        'Nuit nuageuse':'https://images7.alphacoders.com/937/thumb-1920-937486.jpg',
+        'Nuit avec développement nuageux':'https://images5.alphacoders.com/992/thumb-1920-992126.jpg',
+        'Averses de pluie faible':'https://images8.alphacoders.com/714/thumb-1920-714894.jpg',
+        'Pluie faible':'https://images3.alphacoders.com/986/thumb-1920-986363.jpg',
+        'Averses de pluie modérée':'https://images5.alphacoders.com/312/thumb-1920-312372.jpg',
+        'Averses de pluie forte':'https://images.alphacoders.com/108/thumb-1920-1084974.jpg',
+        'Couvert avec averses':'https://images2.alphacoders.com/783/thumb-1920-783245.jpg',
+        'Pluie forte':'https://images2.alphacoders.com/597/thumb-1920-597905.jpg',
+        'Pluie modérée':'https://images7.alphacoders.com/103/thumb-1920-1036817.jpg',
+        'Faiblement orageux':'https://images6.alphacoders.com/854/thumb-1920-854198.jpg',
+        'Orage modéré':'https://images.alphacoders.com/453/thumb-1920-453238.jpg',
+        'Fortement orageux':'https://images2.alphacoders.com/596/thumb-1920-596907.jpg',
+        'Nuit faiblement orageuse':'https://images8.alphacoders.com/697/thumb-1920-697542.jpg',
+        'Averses de neige faible':'https://images5.alphacoders.com/341/thumb-1920-341015.jpg',
+        'Neige faible':'https://images3.alphacoders.com/116/thumb-1920-116827.jpg',
+        'Neige modérée':'https://images7.alphacoders.com/876/thumb-1920-876256.jpg',
+        'Neige forte':'https://images.alphacoders.com/106/thumb-1920-106423.jpg',
+        'Pluie et neige mêlée faible':'https://images.alphacoders.com/106/thumb-1920-106423.jpg',
+        'Pluie et neige mêlée modérée':'https://images6.alphacoders.com/899/thumb-1920-899414.jpg',
+        'Pluie et neige mêlée forte':'https://images6.alphacoders.com/793/thumb-1920-793091.jpg',
+        'Nuit avec averses de neige faible':'https://images2.alphacoders.com/814/thumb-1920-814106.jpg',
+        'Brouillard':'https://images3.alphacoders.com/929/thumb-1920-929605.jpg',
     }
-}
 
-function updatePage(météo){
-    restTemp.textContent = météo.current_condition.tmp + "°C";
-    restCity.textContent = météo.city_info.name;
-    restDate.textContent = "Nous sommes le " + météo.fcst_day_0.day_long +" "+ météo.current_condition.date;
-    restIcon.setAttribute("src",météo.current_condition.icon_big);
-    restWeather.textContent = météo.current_condition.condition;
-    restWindDir.textContent = "Direction du vent: " + météo.current_condition.wnd_dir;
-    restWindSpeed.textContent = "La vitesse du vent est de " + météo.current_condition.wnd_spd + " km/h";
+function updatePage(weather){
+    $temp.textContent = weather.current_condition.tmp + "°C";
+    $city.textContent = weather.city_info.name;
+    $date.textContent = "Nous sommes le " + weather.fcst_day_0.day_long +" "+ weather.current_condition.date;
+    $icon.setAttribute("src",weather.current_condition.icon_big);
+    $weather.textContent = weather.current_condition.condition;
+    $windDir.textContent = "Direction du vent: " + weather.current_condition.wnd_dir;
+    $windSpeed.textContent = "La vitesse du vent est de " + weather.current_condition.wnd_spd + " km/h";
+
+    let result = getImagesForConditions(weather.current_condition.condition);
+    document.body.style.backgroundImage = `url(${result};`
     
-    let restHourly = Object.entries(météo.fcst_day_0.hourly_data);
+    let $hourly = Object.entries(weather.fcst_day_0.hourly_data);
     let html ="";
-    for(i = 0 ; i < restHourly.length ; i++){
-        const [hour, data,] = restHourly[i];
+    for(i = 0 ; i < $hourly.length ; i++){
+        const [hour, data,] = $hourly[i];
         html += `<div class="dataHourly">`+ `<p>`+ hour + " "+`</p>`+  data.TMP2m + "°" +`<p>` +
                 `<img src="`+ data.ICON +`"></img>`+` </p>` +`</div>`;
         hourlyData.innerHTML = html;
@@ -171,20 +99,17 @@ function updatePage(météo){
     dayOne.innerHTML = "";
      for(i = 1 ; i < 5 ; i++){
          const data = "fcst_day_"+[i];
-         dayOne.innerHTML += `<div class="dayByDay col-3">`+ `<p>`+météo[data].date + " " +`</p>` + `<p>` + météo[data].tmin + "°" + `/` +
-                            météo[data].tmax + "°" + `<p>` +  météo[data].day_long +`<p>` +`<img src="`+ météo[data].icon +`"></img>`+` </p>`
-                            +`</div>`;
+                      dayOne.innerHTML += `<div class="dayByDay col-3">`+ `<p>`+weather[data].date + " " +`</p>` + `<p>` + weather[data].tmin + "°" + `/` +
+                      weather[data].tmax + "°" + `<p>` +  weather[data].day_long +`<p>` +`<img src="`+ weather[data].icon +`"></img>`+` </p>`
+                      +`</div>`;
      }
-     condition = météo.current_condition.condition;
-     updateBackground() 
 }
 function show(windContainer) {
     document.getElementById(windContainer).style.visibility = "visible";
-  }
+}
   function hide(windContainer) {
     document.getElementById(windContainer).style.visibility = "hidden";
 }
-
-function updateTitle(){
-    title = city_info.name;
+function getImagesForConditions(key) {
+    return imagesForConditions[key];
 }
--- work.md
+++ work.md
@@ -1,1 +1,3 @@
 Hello, on est le vendredi 21 aout 2020 à 8h et je commence à travailler sur le projet.
+
+maj à 11h50 il fonctionne tjrs après avoir été cassé 40 fois amen :pray:
